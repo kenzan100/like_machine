2873b9bbabd8f3f0d496c0af1408fd909d430f4a
Yuta Okazaki <s04155yo@gmail.com>
2015-11-23 23:55:49 +0900

    basic setup for articles test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123131735) do
+ActiveRecord::Schema.define(version: 20151123144634) do
+
+  create_table "articles", force: true do |t|
+    t.string   "url"
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "provider"
--- spec/features/user_posts_a_link_spec.rb
+++ spec/features/user_posts_a_link_spec.rb
@@ -2,7 +2,14 @@ require 'rails_helper'
 
 describe "submit" do
   it "allows logged in user to post a ink" do
-    FactoryGirl.create(:user)
+    article_link = 'http://myearth.ne.jp'
+    article_title = 'Educational Trading Card Game "My Earth"'
+    user = FactoryGirl.create(:user)
+    page.set_rack_session(user_id: user.id)
     visit submit_path
+    fill_in 'article_link', with: article_link
+    fill_in 'article_title', with: article_title
+    click_on 'submit'
+    expect(page).to have_content article_title
   end
 end
