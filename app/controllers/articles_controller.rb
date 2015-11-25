c9f2aa70773c76db0dccb66b8b563996dd997a2a
Yuta Okazaki <s04155yo@gmail.com>
2015-11-26 00:37:08 +0900

    introduce Bootstrap and better animation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(article_params)
+    @article = current_user.articles.new(article_params)
     @article.posted_at = Time.now
     @article.save!
     redirect_to root_path, notice: 'article created :)'
